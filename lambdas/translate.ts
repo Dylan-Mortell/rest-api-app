d99d8f0e5480f17c7bf17b0ae834d7cb25b66252
Dylan-Mortell <20093313@mail.wit.ie>
2025-03-16 20:51:56 +0000

    Creating amazon translate.ts

--- lambdas/translate.ts
+++ lambdas/translate.ts
@@ -1,6 +1,31 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
     import 'source-map-support/register';
+import apiResponses from './common/apiResponses';
+import * as AWS from 'aws-sdk'
+import { Translate } from 'aws-sdk';
+
+    const translate = new AWS.Translate();
     
     export const handler: APIGatewayProxyHandler = async (event) => {
-        
+        const body = JSON.parse(event.body);
+        const { text, language } = body;
+        if (!text) {
+            return apiResponses._400({ message: 'missing text fom the body' });
+        }
+        if (!language) {
+            return apiResponses._400({ message: 'missing language from the body' });
+        }
+        try {
+            const translateParams: Translate.Types.translateTextRequest = {
+                Text: text,
+                SourceLanguageCode: 'en',
+                TragetLanguageCode: language
+            };
+            const translateMessage = await translate.translateText(translateParams).promise()
+            return apiResponses._200({translateMessage})
+
+        } catch (error) {
+            console.log('error in translation');
+            return apiResponses._400({message: 'unable to translate the message' });
+        }
     };
